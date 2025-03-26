af7c290c2cda21e1d618a6d9f8e80c94a70de8ae
Ticha23 <mutwirigachanja1@gmail.com>
2025-03-26 17:44:38 +0300

    Here is my collection page where we view all changes on my website

--- clothline/urls.py
+++ clothline/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('collection/', views.collection_view, name='collection'),
 ]
 
--- myApp/views.py
+++ myApp/views.py
@@ -8,6 +8,10 @@ def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+def signup(request):
+    return render(request, 'signup.html')
+def login(request):
+    return render(request, 'login.html')
 def collection_view(request):
     item = Item.objects.all()
     return render(request, "collection.html", {"items": item})
